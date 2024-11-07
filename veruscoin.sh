2505b7c52241e558fbdc3288d6879f3ed0522892
RoseCurl <121519650+RoseCurl@users.noreply.github.com>
2024-11-07 12:10:45 +0700

    veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcutz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RVxwfn5TggLnYPgEAGQf8W7kes28QNQGJg.rig101 -p x --cpu 8
